d286fe0aa19ed329b21c02d8e0ab019583340c94
sahil khan <139511550+Sahilkhanbk@users.noreply.github.com>
2025-08-17 15:56:25 +0530

    Add files via upload

---
+++ frontend/src/App.jsx
@@ -0,0 +1,39 @@
+import { useEffect, useState } from 'react'
+import './App.css'
+import axios from 'axios'
+
+
+function App() {
+  const [jokes, setJokes] = useState([])
+
+
+  useEffect(() => {
+    axios.get('/api/jokes')
+      .then((responce) => {
+        setJokes(responce.data)
+      })
+      .catch((error) => {
+        console.log(error)
+      })
+  }, [])
+
+  return (
+    <>
+      <h1> sahil khan touch backend with frontend</h1>
+      <p> jokes {jokes.length}</p>
+
+      {
+        jokes.map((joke) => (
+          <div key={joke.id}>
+            <h3>{joke.title}</h3>
+            <p>{joke.content}</p>
+          </div>
+        ))
+      }
+
+
+    </>
+  )
+}
+
+export default App
